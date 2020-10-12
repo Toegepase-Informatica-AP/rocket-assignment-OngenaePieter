8838b1ffea5ccb96b4cb73717c9e30959cfcd1d0
OngenaePieter <pieterbracke@hotmail.com>
2020-10-12 11:12:07 +0200

    Rocket Assignment
    Pieter Ongenae

---
+++ Rocket Assignment/Assets/Scripts/EndGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndGame : MonoBehaviour
+{
+    public string endgameText = "";
+
+    public TextMeshProUGUI textEnd;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        textEnd.text = endgameText.ToString();
+    }
+}
---
+++ Rocket Assignment/Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+
+    float rotationLeftRight = 0;
+    public float speed = 0;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0, speed, 0);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotationLeftRight -= 0.01f;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotationLeftRight += 0.01f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            rotationLeftRight = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            rotationLeftRight = 0;
+        }
+
+        transform.Rotate(rotationLeftRight, 0, 0, Space.Self);
+    }
+}
